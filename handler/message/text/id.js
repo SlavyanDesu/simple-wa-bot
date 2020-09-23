cce0b20d8007438bf736cb2e4781fe4fb3037dbc
HungXiao007 <heikalsyahshiddiq@gmail.com>
2020-09-24 01:26:40 +0700

    Hidden menu

--- handler/message/text/id.js
+++ handler/message/text/id.js
@@ -1,32 +1,32 @@
 exports.textTnC = () => {
     return `
-    MIT License
+MIT License
 
-    Copyright (c) 2020 Heikal Syah Shiddiq
+Copyright (c) 2020 Heikal Syah Shiddiq
     
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
     
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
     
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 `
 }
 
 exports.textMenu = (pushname) => {
     return `
-⚠ [ *ALL YOU CAN USE* ] ⚠ 
+⚠️ [ *ALL YOU CAN USE* ] ⚠️
 Berikut command yang tersedia:
 
 Downloader (TWITTER DAN FACEBOOK UPDATE):
@@ -53,14 +53,14 @@ Penggunaan: kirim gambar dengan caption *$sticker* atau balas gambar yang sudah 
 
 Fun:
 1. *$say*
-Bikin gw mengatakan apa yang lu katakan. <> Abaikan.
+It's just a say command, whatd'ya expect?. <> Abaikan.
 Aliases: *talk*
-Penggunaan: *$say* <teks lu>
+Penggunaan: *$say* <teks>
 
 2. *$ask*
-Tanya gw tentang apa aja, gw jawab sebisanya. <> Abaikan.
+Tanya tentang apa saja dan saya akan menjawab secara random. <> Abaikan.
 Aliases: *8ball*
-Penggunaan:: *$ask* <pertanyaan lu>
+Penggunaan:: *$ask* <pertanyaan>
 
 3. *$reverse*
 .nasilut nikilaB
@@ -68,7 +68,7 @@ Aliases: -
 Penggunaan: *$reverse* <teks lu>
 
 4. *$roll*
-Buat ngeroll dadu.
+Lempar dadu.
 Aliases: *dice*
 Penggunaan: *$roll*
 
@@ -77,24 +77,35 @@ Lempar koin.
 Aliases: *coin* - *flip*
 Penggunaan: *$coinflip*
 
+~6. $ascii~
+~Buat teks mu menjadi ASCII art.~
+~Aliases: -~
+~Penggunaan: *$ascii* <teks lu>~
+*WIP*
+
+7. *$randmeme*
+Kirim random meme dari reddit.
+Aliases: *reddit*
+Penggunaan: *$randmeme*
+
 Utilities:
 1. *$ping*
-Buat ngecek respon bot.
+Cek ping.
 Alias: *speed*
 Penggunaan: *$ping*
 
 2. *$server*
-Liat spesifikasi server.
+Cek spesifikasi server.
 Aliases: -
 Penggunaan: *$server*
 
 3. *$eval*
-Buat evaluates string JavaScript tanpa terminal anjay canggih. <> Abaikan.
+Buat evaluates string JavaScript. <> Abaikan.
 Alias: *ev*
 Penggunaan: *$eval* <string>
 
 4. *$clock*
-Buat ngecek waktu Indonesia.
+Cek waktu Indonesia sekitar.
 Aliases: *waktu* - *jam*
 Penggunaan: *$clock*
 
@@ -110,9 +121,20 @@ Penggunaan: *$menu*
 `
 }
 
+exports.hiddenMenu = () => {
+    return `
+🔞 [ *NSFW* ] 🔞
+Command untuk para lort:
+1. *$ecchi*
+Kirim foto lewd berbasis dari r/ecchi.
+Aliases: -
+Penggunaan: *$ecchi*
+    `
+}
+
 exports.textAdmin = () => {
     return `
-⚠ [ *ADMIN ONLY* ] ⚠ 
+⚠️ [ *ADMIN ONLY* ] ⚠️
 Gunakan command ini jika admin terlalu malas:
 
 1. *$kick*
