4ebbc9f9d3ffca653f1cb94624c4ec3a35227762
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2020-09-26 11:40:57 +0700

    Update options.js

--- utils/options.js
+++ utils/options.js
@@ -1,6 +1,27 @@
 const fs = require('fs')
 
 module.exports = options = (headless, start) => {
+    const chromePath = {
+        win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Windows 32 bit
+        win64: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', // Windows 64 bit
+        linuxChrome: '/usr/bin/google-chrome-stable', // Linux - Chrome
+        linuxChromium: '/usr/bin/chromium-browser', // Linux - Chromium
+        darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' // MacOS
+    }
+    if (fs.existsSync(chromePath.win32)) {
+        execPath = chromePath.win32
+    } else if (fs.existsSync(chromePath.win64)) {
+        execPath = chromePath.win64
+    } else if (fs.existsSync(chromePath.linuxChrome)) {
+        execPath = chromePath.linuxChrome
+    } else if (fs.existsSync(chromePath.linuxChromium)) {
+        execPath = chromePath.linuxChromium
+    } else if (process.platform === 'darwin') {
+        execPath = chromePath.darwin
+    } else {
+        console.error(new Error('Google Chrome is not installed!'))
+        process.exit(1)
+    }
     const options = {
         headless: headless,
         qrRefreshS: 20,
@@ -19,7 +40,6 @@ module.exports = options = (headless, start) => {
             '--disable-cache',
             '--disable-application-cache',
             '--disable-offline-load-stale-cache',
-            '--disable-extensions',
             '--disk-cache-size=0'
         ]
     }
