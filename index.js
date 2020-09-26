c0e2b6c37c5c1b5ee7ae3df943f42167279a5135
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2020-09-26 12:03:57 +0700

    Nothing

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const options = require('./utils/options')
 const msgHandler = require('./handler/message')
 
 const start = (client = new Client()) => {
-    console.log('[DEV]', color('Slavyan', 'orange'))
+    console.log('[DEV]', color('Slavyan', 'orange')) // Change your name and color here
     console.log('[CLIENT] Bot is now online!')
 
     // Force it to keep the current session
     client.onStateChanged((state) => {
         console.log('[Client State]', state)
+        if (state === 'UNPAIRED') client.forceRefocus()
         if (state === 'CONFLICT') client.forceRefocus()
     })
+    
+    // Set all received message to seen
+    client.onAck((x => {
+        const { from, to, ack } = x
+        if (x !== 3) client.sendSeen(to)
+    }))
 
     // Listening on message
     client.onMessage((message) => {
@@ -27,6 +34,7 @@ const start = (client = new Client()) => {
     })
 }
 
-create('Slavyan', options(true, start))
+// Creating Slavyan.data.json
+create('Slavyan', options(true, start)) // Change your session name here
     .then((client) => start(client))
     .catch((err) => new Error(err))
